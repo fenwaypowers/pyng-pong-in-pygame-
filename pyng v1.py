8b25623d9f7f9c439cc755e5a1ab76c8875db06b
Fenway Powers <31604245+fenwaypowers@users.noreply.github.com>
2021-05-24 09:08:03 -0400

    Update pyng v1.py

--- pyng v1.py
+++ pyng v1.py
@@ -1,4 +1,6 @@
 import pygame, random, time
+import pygame.freetype
+
 pygame.init()
 width = 600
 height = 600
@@ -13,12 +15,21 @@ pygame.display.flip()
 key_input = pygame.key.get_pressed()
 clock = pygame.time.Clock()
 ball = pygame.draw.rect(screen, white, ((275,275),(40,40)))
-leftp = pygame.draw.rect(screen, white, ((5,300),(15,120)))
+leftp = pygame.draw.rect(screen, white, ((5,220),(15,120)))
 rightp = pygame.draw.rect(screen, white, ((580,0),(15,600)))
 leftp_up=False
 leftp_down=False
 direction=1
-ydirection=1
+ydirection=random.choice([1,-1])
+
+left_score=0
+right_score=0
+
+GAME_FONT = pygame.freetype.Font("Roboto-Bold.ttf", 48)
+text_surface, rect = GAME_FONT.render("Hello World!", white)
+screen.blit(text_surface, (40, 250))
+
+
 
 def movepaddle(paddle,downorup):
     increment=12.5
@@ -26,26 +37,52 @@ def movepaddle(paddle,downorup):
     newy = paddle.y+(increment*downorup)
     pygame.draw.rect(screen, black, ((paddle.x,paddle.y),(15,120)))
     pygame.display.update()
-    newpaddle = pygame.draw.rect(screen, white, ((paddle.x,newy),(15,120)))
-    if newpaddle.y > 480:
+    if newy > 490:
         newpaddle = paddle
+    else:
+        newpaddle = pygame.draw.rect(screen, white, ((paddle.x,newy),(15,120)))
     return newpaddle
 
 def ball_move(ball,leftp,rightp,direction,ydirection):
+    increment=5
     
-    if (ball.y in range(leftp.y-40,leftp.y + leftp.height+20) and ball.x == leftp.x) or (ball.y in range(rightp.y-40,rightp.y + rightp.height+20) and ball.x == (rightp.x-20)):
+    if (ball.y in range(leftp.y-40,leftp.y + leftp.height+20) and ball.x == leftp.x or ball.x > leftp.x and ball.x < (leftp.x+5)) or (ball.y in range(rightp.y-40,rightp.y + rightp.height+20) and ball.x == (rightp.x-20)):
         direction*=-1
-    newx=(ball.x)+(5*direction)
+    newx=(ball.x)+(increment*direction)
 
-    if ball.y in range(leftp.y-40,leftp.y + int((0.5*leftp.height)+20)) and ball.x == leftp.x:
-        print("up")
+    if ball.y in range(leftp.y-40,leftp.y + int((0.5*leftp.height))) and ball.x == leftp.x:
+        ydirection=-1
+    elif ball.y in range(int(1.5*(leftp.y-40)),leftp.y+leftp.height) and ball.x == leftp.x:
+        ydirection=1
 
+    if ball.y in range(rightp.y-40,rightp.y + int((0.5*rightp.height))) and ball.x == rightp.x:
+        ydirection=-1
+    elif ball.y in range(int(1.5*(rightp.y-40)),rightp.y+rightp.height) and ball.x == rightp.x:
+        ydirection=1
 
-    
+    if ball.y == 560 or ball.y == 0:
+        ydirection*=-1
+
+    newy=ball.y+((increment)*ydirection)
+
     pygame.draw.rect(screen, black, ((ball.x,ball.y),(40,40)))
     pygame.display.update()
-    newball = pygame.draw.rect(screen, white, ((newx,ball.y),(40,40)))
+    newball = pygame.draw.rect(screen, white, ((newx,newy),(40,40)))
     return newball, direction, ydirection
+
+def gameover():
+    time.sleep(0.5)
+    screen.fill(black)
+    ball = pygame.draw.rect(screen, white, ((275,275),(40,40)))
+    leftp = pygame.draw.rect(screen, white, ((5,300),(15,120)))
+    rightp = pygame.draw.rect(screen, white, ((580,0),(15,600)))
+    leftp_up=False
+    leftp_down=False
+    direction=1
+    ydirection=1
+    time.sleep(0.5)
+
+    return ball, leftp, rightp, leftp_up, leftp_down, direction, ydirection
 
 pygame.display.update()
 
@@ -77,5 +114,11 @@ while True:
 
     pygame.draw.rect(screen, white, ((580,rightp.y),(15,600)))
     pygame.draw.rect(screen, white, ((leftp.x,leftp.y),(15,120)))
+
+    if ball.x < leftp.x:
+        right_score+=1
+        ball, leftp, rightp, leftp_up, leftp_down, direction, ydirection = gameover()
+    if ball.x > rightp.x:
+        ball, leftp, rightp, leftp_up, leftp_down, direction, ydirection = gameover()
      
     pygame.display.update()
